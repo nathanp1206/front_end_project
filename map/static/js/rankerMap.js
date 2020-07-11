a8415b5f783fcee2bbb6157c3c6ecc7a95a3234f
David Mitchell <dmitchell217@gmail.com>
2020-07-11 16:53:55 -0400

    combined rankMap with other layers

--- map/static/js/rankerMap.js
+++ map/static/js/rankerMap.js
@@ -1,24 +1,24 @@
 const rankerMap = () => {
 // Creating map object
-var myMap = L.map("map", {
-    center: [33.7490, -84.3880],
-    zoom: 11
-  });
+// var myMap = L.map("map", {
+//     center: [33.7490, -84.3880],
+//     zoom: 11
+//   });
 
-  // Adding tile layer
-  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
-    maxZoom: 18,
-    zoomOffset: -1,
-    id: "mapbox/streets-v11",
-    accessToken: API_KEY
-  }).addTo(myMap);
+//   // Adding tile layer
+//   L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+//     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+//     tileSize: 512,
+//     maxZoom: 18,
+//     zoomOffset: -1,
+//     id: "mapbox/streets-v11",
+//     accessToken: API_KEY
+//   }).addTo(myMap);
   
   // Use this link to get the geojson data.
   var link = "static/data/mlp.geojson"
   
-  // Function that will determine the color of a neighborhood based on the borough it belongs to
+  // Function that will determine the color of a zipcode based on its ranking
   function chooseColor(ranking) {
     switch (ranking) {
     case "A":
@@ -35,11 +35,11 @@ var myMap = L.map("map", {
       return "black";
     }
   }
-  
+  var rankLayer;
   // Grabbing our GeoJSON data..
   d3.json(link, function(data) {
     // Creating a geoJSON layer with the retrieved data
-    L.geoJson(data, {
+    rankLayer = L.geoJson(data, {
       // Style each feature (in this case a neighborhood)
       style: function(feature) {
         return {
@@ -78,5 +78,6 @@ var myMap = L.map("map", {
   
       }
     }).addTo(myMap);
+    controlObject.addOverlay(rankLayer,"Rankings")
   });
 }
