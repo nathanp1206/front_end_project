3b3661f236597e2936f3274871fed47a0417b274
David Mitchell <dmitchell217@gmail.com>
2020-07-10 20:32:49 -0400

    crime, map, data

--- src/static/js/FilterableMap.js
+++ src/static/js/FilterableMap.js
@@ -54,6 +54,52 @@ info.onAdd = function() {
 info.addTo(myMap);
 
 // create layers...
+var link = "static/data/mlp.geojson"
+
+// Grabbing our GeoJSON data..
+d3.json(link, function(data) {
+  // Creating a geoJSON layer with the retrieved data
+  L.geoJson(data, {
+    // Style each feature (in this case a neighborhood)
+    style: () => {
+      return {
+        color: "grey",
+        fillColor: "blue",
+        fillOpacity: 0.05,
+        weight: 1.0
+      };
+    },
+    // Called on each feature
+    onEachFeature: function(feature, layer) {
+      // Set mouse events to change map styling
+      layer.on({
+        // When a user's mouse touches a map feature, the mouseover event calls this function, that feature's opacity changes to 90% so that it stands out
+        mouseover: function(event) {
+          layer = event.target;
+          layer.setStyle({
+            fillOpacity: 0.1
+          });
+        },
+        // When the cursor no longer hovers over a map feature - when the mouseout event occurs - the feature's opacity reverts back to 50%
+        mouseout: function(event) {
+          layer = event.target;
+          layer.setStyle({
+            fillOpacity: 0.05
+          });
+        },
+        // When a feature (neighborhood) is clicked, it is enlarged to fit the screen
+        click: function(event) {
+          myMap.fitBounds(event.target.getBounds());
+        }
+      });
+      // Giving each feature a pop-up with information pertinent to it
+      layer.bindPopup("<h1>" + feature.properties.ZipCode);
+
+    }
+  }).addTo(myMap);
+});
+
+
 // crimes
 
 var link = "static/data/crime_data.csv"
@@ -200,10 +246,3 @@ let edMarkers = L.markerClusterGroup();
     controlObject.addOverlay(edMarkers,"Schools")
 }
 fetchSchools2()
-
-
-
-
-
-
-
---
+++ src/static/js/zipCodeFetchData.js
@@ -0,0 +1,9 @@
+
+// to be called before map handling: generates all the data required for visualization
+const fetchData = () => {
+    // grab the mlp from the geojson
+    // Decision: grab the data from a global variable created after the schooldigger call or call it first here
+    // grab the crime data right from the csv
+
+    // return an object with all three of these data points
+}
