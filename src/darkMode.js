6cb16e4ab30c523e85fc01debb42020f2bf051a8
Hunt3r1206 <npatton1206@gmail.com>
2020-07-15 18:16:47 -0400

    Added Dark Mode
    Made Dark Mode default and added a switcher for light mode

---
+++ src/darkMode.js
@@ -0,0 +1,14 @@
+export function whiteMode() {
+    var element = document.body;
+    element.classList.toggle("white-mode");
+    var element2 = document.getElementById("buttons");
+    element2.classList.toggle("white-mode");
+    var element3 = document.getElementById("buttons1");
+    element3.classList.toggle("white-mode");
+    var element4 = document.getElementById("buttons2");
+    element4.classList.toggle("white-mode");
+    var element5 = document.getElementById("buttons3");
+    element5.classList.toggle("white-mode");
+    // var element6 = document.getElementById("input1");
+    // element6.style.backgroundColor("white")
+}
